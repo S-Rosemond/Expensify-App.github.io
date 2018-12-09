e0acaf6801df93dddc2f625192e518cda0a13a22
Samuel <js.rosemond@gmail.com>
2018-12-09 13:42:54 -0500

    Client side routes NavLink and Link

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './Styles/styles.scss';
 
@@ -17,22 +17,54 @@ const HelpPage = () => {
 	return <div>This is my Help Page</div>;
 };
 const NotFoundPage = () => {
-	return <div>Error! 404 Page not found!</div>;
+	return (
+		<div>
+			Error! 404 Page not found!
+			<Link to="/">Go home</Link>
+		</div>
+	);
+};
+const Header = () => {
+	return (
+		<header>
+			<h1>Expensify</h1>
+			<NavLink activeClassName="is-active" to="/" exact={true}>
+				Go home
+			</NavLink>
+			<br />
+			<NavLink activeClassName="is-active" to="/create" exact={true}>
+				Add
+			</NavLink>
+			<br />
+			<NavLink activeClassName="is-active" to="/edit" exact={true}>
+				Edit
+			</NavLink>
+			<br />
+			<NavLink activeClassName="is-active" to="/help" exact={true}>
+				Get help
+			</NavLink>
+			<br />
+		</header>
+	);
 };
 
 const routes = (
 	<BrowserRouter>
-		<Switch>
-			<Route path="/" component={ExpenseDashboardPage} exact={true} />
+		<div>
+			<Header />
 
-			<Route path="/create" component={AddExpensePage} exact={true} />
+			<Switch>
+				<Route path="/" component={ExpenseDashboardPage} exact={true} />
 
-			<Route path="/edit" component={EditExpensePage} exact={true} />
+				<Route path="/create" component={AddExpensePage} exact={true} />
 
-			<Route path="/help" component={HelpPage} exact={true} />
+				<Route path="/edit" component={EditExpensePage} exact={true} />
 
-			<Route component={NotFoundPage} />
-		</Switch>
+				<Route path="/help" component={HelpPage} exact={true} />
+
+				<Route component={NotFoundPage} />
+			</Switch>
+		</div>
 	</BrowserRouter>
 );
 
