8ae82ec775fbfe8ed2b94860c28e4a5bd0ffd412
Samuel <js.rosemond@gmail.com>
2018-12-12 06:05:47 -0500

    Redux Actions

--- src/playground/Redux-101.js
+++ src/playground/Redux-101.js
@@ -1,7 +1,41 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
+//second param is action
+const store = createStore((state = { count: 0 }, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return {
+				count: state.count + 1
+			};
+		case 'DECREMENT':
+			return {
+				count: state.count - 1
+			};
+		case 'RESET':
+			return { count: 0 };
+		default:
+			return state;
+	}
 	return state;
+});
+
+console.log(store.getState());
+
+//ACTIONS_OTHERWORD_OTHERWORDAGAIN
+
+//INCREMENT
+store.dispatch({
+	type: 'INCREMENT'
+});
+console.log(store.getState());
+//RESET
+store.dispatch({
+	type: 'RESET'
+});
+
+//DECREMENT
+store.dispatch({
+	type: 'DECREMENT'
 });
 
 console.log(store.getState());
