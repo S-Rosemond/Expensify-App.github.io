728ed0c665b219617d70b7f6798d5f8471fad943
Samuel <js.rosemond@gmail.com>
2018-12-12 06:56:29 -0500

    subscribe + dynamic actions

--- src/playground/Redux-101.js
+++ src/playground/Redux-101.js
@@ -2,40 +2,53 @@ import { createStore } from 'redux';
 
 //second param is action
 const store = createStore((state = { count: 0 }, action) => {
+	//Could use global value (DRY) but course uses local
 	switch (action.type) {
 		case 'INCREMENT':
+			const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
 			return {
-				count: state.count + 1
+				count: state.count + incrementBy
 			};
 		case 'DECREMENT':
+			const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
 			return {
-				count: state.count - 1
+				count: state.count - decrementBy
 			};
+		case 'SET':
+			return { count: action.count };
 		case 'RESET':
 			return { count: 0 };
 		default:
 			return state;
 	}
-	return state;
 });
 
-console.log(store.getState());
+//Subscribe. return func (ref)= any(); : stop ||unsub
+const unsubscribe = store.subscribe(() => {
+	console.log(store.getState());
+});
 
-//ACTIONS_OTHERWORD_OTHERWORDAGAIN
+store.dispatch({
+	type: 'INCREMENT',
+	incrementBy: 5
+});
 
-//INCREMENT
 store.dispatch({
 	type: 'INCREMENT'
 });
-console.log(store.getState());
-//RESET
+
 store.dispatch({
 	type: 'RESET'
 });
 
-//DECREMENT
 store.dispatch({
 	type: 'DECREMENT'
 });
-
-console.log(store.getState());
+store.dispatch({
+	type: 'DECREMENT',
+	decrementBy: 10
+});
+store.dispatch({
+	type: 'SET',
+	count: 101
+});
