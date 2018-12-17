4436042dba1e5a73904a3ecc932062542f9e2fee
Samuel <js.rosemond@gmail.com>
2018-12-17 15:55:32 -0500

    Redux store update. Not timed = completed on my own- no blanks!

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -36,10 +36,23 @@ const setTextFilter = (text = '') => ({
 });
 
 //SORT_BY_DATE
+const sortByDate = () => ({
+	type: 'SORT_BY_DATE'
+});
 //SORT_BY_AMOUNT
+const sortByAmount = () => ({
+	type: 'SORT_BY_AMOUNT'
+});
 //SET_START_DATE
+const setStartDate = value => ({
+	type: 'SET_START_DATE',
+	value
+});
 //SET_END_DATE
-
+const setEndDate = value => ({
+	type: 'SET_END_DATE',
+	value
+});
 //EXPENSES REDUCER
 
 const expensesReducerDefault = [];
@@ -72,6 +85,14 @@ const filtersReducer = (state = filtersReducerDefault, action) => {
 		case 'SET_TEXT_FILTER':
 			console.log('action:', action);
 			return { ...state, text: action.text };
+		case 'SORT_BY_DATE':
+			return { ...state, sortBy: 'date' };
+		case 'SORT_BY_AMOUNT':
+			return { ...state, sortBy: 'amount' };
+		case 'SET_START_DATE':
+			return { ...state, startDate: action.value };
+		case 'SET_END_DATE':
+			return { ...state, endDate: action.value };
 		default:
 			return state;
 	}
@@ -89,14 +110,22 @@ store.subscribe(() => {
 	console.log(store.getState());
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffe', amount: 300 }));
+// const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+// const expenseTwo = store.dispatch(addExpense({ description: 'Coffe', amount: 300 }));
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter('rent'));
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+store.dispatch(setStartDate(125));
+store.dispatch(setEndDate(500));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate());
 
 const demoState = {
 	expenses: [
@@ -122,9 +151,9 @@ const demoState = {
 // };
 
 // let objClone = { ...user };
-// console.log(objClone);
-// console.log({
-// 	...user,
-// 	location: 'Philadelphia',
-// 	age: 27
-// });
+// console.log('clone:', objClone);
+// objClone.name = 'Sam';
+// console.log('clone:', objClone);
+// console.log('user:', user);
+// let secondObjClone = { ...objClone, ...user };
+// console.log('second clone:', secondObjClone);
